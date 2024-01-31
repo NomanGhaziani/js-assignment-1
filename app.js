693647b04e9c30568c15b2586bfe27e3df143b91
NomanGhaziani <153339067+NomanGhaziani@users.noreply.github.com>
2024-01-31 08:50:49 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+alert("Welcome to Our Website");
+alert("Error! Please Enter a valid password.");
+var line = "Its first line \n and Its another line";
+alert(line);
+alert("First message");
+alert("Second message");
+alert("Third message");
+document.write("Question No-6: Answer is D option" + "<br>");
+var username = "noman";
+var MyName = "Noman Attari";
+alert(MyName);
+var message = "Hello World";
+alert(message);
+var student = {
+    name: "Noman",
+    id: 123
+};
+alert(student.name);
+alert(student.id);
+var email ="92nomanattari@gmail.com";
+alert("Email Address:" + email);
+document.write("Display through js" + "<br>");
+alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”");
+document.write("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”" + "<br>");
+var Age ="26";
+alert(Age);
+var birthyear =1995;
+document.write(birthyear + "<br>");
